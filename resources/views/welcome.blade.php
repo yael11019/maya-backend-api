e3b04161dec93f32523ea3d9d97ba33d47bb5f94
yael11019 <yael110@live.com.mx>
2025-10-08 17:33:53 -0600

    Fixing issues on prod

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
-            @if (Route::has('login'))
+            @if (route::has('login'))
                 <nav class="flex items-center justify-end gap-4">
                     @auth
                         <a
@@ -38,7 +38,7 @@
                             Log in
                         </a>
 
-                        @if (Route::has('register'))
+                        @if (route::has('register'))
                             <a
                                 href="{{ route('register') }}"
                                 class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
@@ -270,7 +270,7 @@
             </main>
         </div>
 
-        @if (Route::has('login'))
+        @if (route::has('login'))
             <div class="h-14.5 hidden lg:block"></div>
         @endif
     </body>
